fe0c67b9b9545f8f0d477cae5a068973a4cf78f8
Unknown <alexander.zeitler@pdmlab.com>
2017-02-17 20:28:43 +0100

    Extract public key from x5c x509 certificate chain

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const fs = require('fs');
 const NodeRSA = require('node-rsa');
 const path = require('path');
-const x5cRaw = fs.readFileSync(path.join(__dirname, 'x5c.crt'));
-const x5c = Buffer.from(x5cRaw, 'base64', Buffer.byteLength(x5cRaw, 'base64'));
-const key = new NodeRSA(x5c, 'public');
+const x509 = require('x509');
+
+const parsedKey = x509.parseCert(path.join(__dirname, 'x5c.crt'));
+const key = new NodeRSA();
+
+key.importKey({
+    n: new Buffer(parsedKey.publicKey.n, 'hex'),
+    e: parseInt(parsedKey.publicKey.e, 10)
+}, 'components-public');
+
 const exportedKey = key.exportKey('public');
 
 console.log(exportedKey);
